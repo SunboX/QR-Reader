a28b9e06776e081692e761cf4d35fb22c74546b1
André Fiedler <andre.fiedler@me.com>
2015-01-06 22:58:16 +0100

    Implementing CameraControl API

---
+++ CameraCapabilities.js
@@ -0,0 +1,201 @@
+var CameraCapabilities = { 
+  previewSizes: [
+    {
+      height: 480,
+      width: 640
+    },
+    {
+      height: 432,
+      width: 576
+    },
+    {
+      height: 320,
+      width: 480
+    },
+    {
+      height: 240,
+      width: 432
+    },
+    {
+      height: 288,
+      width: 384
+    },
+    {
+      height: 288,
+      width: 352
+    },
+    {
+      height: 240,
+      width: 320
+    },
+    {
+      height: 160,
+      width: 240
+    },
+    {
+      height: 144,
+      width: 176
+    }
+  ], 
+  pictureSizes: [
+    {
+      height: 1536,
+      width: 2048
+    },
+    {
+      height: 1080,
+      width: 1920
+    },
+    {
+      height: 1200,
+      width: 1600
+    },
+    {
+      height: 768,
+      width: 1280
+    },
+    {
+      height: 720,
+      width: 1280
+    },
+    {
+      height: 768,
+      width: 1024
+    },
+    {
+      height: 600,
+      width: 800
+    },
+    {
+      height: 480,
+      width: 800
+    },
+    {
+      height: 480,
+      width: 640
+    },
+    {
+      height: 288,
+      width: 352
+    },
+    {
+      height: 240,
+      width: 320
+    },
+    {
+      height: 144,
+      width: 176
+    }
+  ], 
+  thumbnailSizes: [
+    {
+      height: 288,
+      width: 512
+    },
+    {
+      height: 288,
+      width: 480
+    },
+    {
+      height: 288,
+      width: 432
+    },
+    {
+      height: 384,
+      width: 512
+    },
+    {
+      height: 288,
+      width: 352
+    },
+    {
+      height: 0,
+      width: 0
+    }
+  ], 
+  videoSizes: [
+    {
+      height: 480,
+      width: 640
+    },
+    {
+      height: 432,
+      width: 576
+    },
+    {
+      height: 320,
+      width: 480
+    },
+    {
+      height: 240,
+      width: 432
+    },
+    {
+      height: 288,
+      width: 384
+    },
+    {
+      height: 288,
+      width: 352
+    },
+    {
+      height: 240,
+      width: 320
+    },
+    {
+      height: 160,
+      width: 240
+    },
+    {
+      height: 144,
+      width: 176
+    }
+  ], 
+  fileFormats: [
+    "jpeg",
+    "raw"
+  ], 
+  whiteBalanceModes: [
+    "auto",
+    "incandescent",
+    "fluorescent",
+    "daylight",
+    "cloudy-daylight"
+  ], 
+  sceneModes: [
+    "auto",
+    "asd",
+    "action",
+    "portrait",
+    "landscape",
+    "night",
+    "night-portrait",
+    "theatre",
+    "beach",
+    "snow",
+    "sunset",
+    "steadyphoto",
+    "fireworks",
+    "sports",
+    "party",
+    "candlelight",
+    "backlight",
+    "flowers",
+    "AR"
+  ], 
+  effects: [
+    "none",
+    "mono",
+    "negative",
+    "solarize",
+    "sepia",
+    "posterize",
+    "whiteboard",
+    "blackboard",
+    "aqua"
+  ], 
+  flashModes: [], 
+  focusModes: [
+    "infinity"
+  ]
+};
